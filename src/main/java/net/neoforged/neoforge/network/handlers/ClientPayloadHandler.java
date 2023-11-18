c3fc54def6197852c175b903e8ff1de837e908b3
Marc Hermans <marc.hermans@ldtteam.com>
2023-11-18 22:37:02 +0100

    Start entity implementation.

--- src/main/java/net/neoforged/neoforge/network/handlers/ClientPayloadHandler.java
+++ src/main/java/net/neoforged/neoforge/network/handlers/ClientPayloadHandler.java
@@ -2,18 +2,26 @@ package net.neoforged.neoforge.network.handlers;
 
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import io.netty.buffer.Unpooled;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.common.util.LogicalSidedProvider;
+import net.neoforged.neoforge.entity.IEntityAdditionalSpawnData;
 import net.neoforged.neoforge.network.ConfigSync;
 import net.neoforged.neoforge.network.handling.ConfigurationPayloadContext;
-import net.neoforged.neoforge.network.payload.ConfigFilePayload;
-import net.neoforged.neoforge.network.payload.FrozenRegistryPayload;
-import net.neoforged.neoforge.network.payload.FrozenRegistrySyncCompletePayload;
-import net.neoforged.neoforge.network.payload.FrozenRegistrySyncStartPayload;
+import net.neoforged.neoforge.network.handling.PlayPayloadContext;
+import net.neoforged.neoforge.network.payload.*;
 import net.neoforged.neoforge.registries.ForgeRegistry;
 import net.neoforged.neoforge.registries.GameData;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -58,5 +66,33 @@ public class ClientPayloadHandler {
     
     public void handle(ConfigurationPayloadContext context, ConfigFilePayload payload) {
         ConfigSync.INSTANCE.receiveSyncedConfig(payload.contents(), payload.fileName());
+    }
+    
+    public void handle(PlayPayloadContext context, AdvancedAddEntityPayload msg) {
+        EntityType<?> type = BuiltInRegistries.ENTITY_TYPE.byId(msg.typeId());
+        Optional<Level> world = LogicalSidedProvider.CLIENTWORLD.get(ctx.getDirection().getReceptionSide());
+        Entity e = world.map(w -> type.customClientSpawn(msg, w)).orElse(null);
+        if (e == null) {
+            return;
+        }
+        
+        /*
+         * Sets the postiion on the client, Mirrors what
+         * Entity#recreateFromPacket and LivingEntity#recreateFromPacket does.
+         */
+        e.syncPacketPositionCodec(msg.posX(), msg.posY(), msg.posZ());
+        e.absMoveTo(msg.posX(), msg.posY(), msg.posZ(), (msg.yaw() * 360) / 256.0F, (msg.pitch() * 360) / 256.0F);
+        e.setYHeadRot((msg.headYaw() * 360) / 256.0F);
+        e.setYBodyRot((msg.headYaw() * 360) / 256.0F);
+        
+        e.setId(msg.entityId());
+        e.setUUID(msg.uuid());
+        world.filter(ClientLevel.class::isInstance).ifPresent(w -> ((ClientLevel) w).addEntity(e));
+        e.lerpMotion(msg.velX() / 8000.0, msg.velY() / 8000.0, msg.velZ() / 8000.0);
+        if (e instanceof IEntityAdditionalSpawnData entityAdditionalSpawnData) {
+            final FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.wrappedBuffer(msg.customPayload()));
+            entityAdditionalSpawnData.readSpawnData(buf);
+            buf.release();
+        }
     }
 }
